a6f71698e959cbfe5044402a3b3f924be2923ac5
isaac <ia464@cam.ac.uk>
2025-05-27 16:54:04 +0100

    Add a fixture to setup the prediction class
    This is a fixture which sets up the prediction class with real data.
    It currently makes use of the prepare function from main_forecast.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+from lib.forecast import Simulation, Predictions, load_ts
+from main_forecast import prepare
 
 
 @pytest.fixture()
@@ -26,3 +28,25 @@ def setup_simulation_class(request):
     )
 
     return simu
+
+
+@pytest.fixture()
+def setup_prediction_class(request):
+    """
+    Fixture to set up a prediction class
+    """
+    # TODO: Reminder about handling index of tuple term, filename
+    term = request.param
+    path = "tests/data/nemo_data_e3/"
+    start = 20
+    end = 50
+    ye = True
+    comp = 0.9
+
+    prepare(term, path, start, end, ye, comp)
+
+    df, infos = load_ts(f"{path}/simu_prepared/{term[0]}", term[0])
+
+    simu_ts = Predictions(term[0], df, infos)
+
+    return simu_ts
