83d46f3a94d088a7ec02cfe2dc6881837d5e38d2
Matt Archer <ma595@cam.ac.uk>
2025-11-18 17:42:18 +0000

    chore: remove unused files
    - restart code and scripts
    - optimization step
    - tools directory
    - restart notebook
    
    These scripts/tools sit outside the forecasting pipeline.

--- tools/CMIP_to_xarray.sh
+++
@@ -1,9 +0,0 @@
-####
-# Hopefully you made sure your restart files have been made modifiable ( chmod 666 should be fine)
-#
-####
-
-for i in "$@"; do
-  	ncrename -v y,yy $i
-	ncrename -v x,xx $i
-done
